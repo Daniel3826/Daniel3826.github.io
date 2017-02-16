36069dab62f1e4f673334f5d8d8345c18779c5e3
Daniel3826 <dani.aguilar1201@gmail.com>
2017-02-16 14:10:08 -0600

    Create 20162.js

---
+++ 20162.js
@@ -0,0 +1,14 @@
+var miCanvas = document.getElementById("miCanvas")
+var contexto = miCanvas.getContext("2d");
+contexto.fillStyle = "red";
+contexto.fillRect(30, 30, 50, 50);
+
+var miCanvas2 = document.getElementById("miCanvas")
+var contexto2 = miCanvas.getContext("2d");
+contexto.fillStyle = "blue";
+contexto.fillRect(40, 40, 60, 60);
+
+var miCanvas2 = document.getElementById("miCanvas")
+var contexto2 = miCanvas.getContext("2d");
+contexto.fillStyle = "yellow";
+contexto.fillRect(50, 50, 70, 70);
