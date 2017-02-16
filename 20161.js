c8a2ee14561cf5398f4395b7756c5fb653f6fe89
Daniel3826 <dani.aguilar1201@gmail.com>
2017-02-16 13:45:00 -0600

    Update 20161.js

--- 20161.js
+++ 20161.js
@@ -1,18 +1,4 @@
-<!Doctype html>
-  <html lang="es">
-   <head>
-     <meta charset="utf-8">
-     <title>Ejemplo 2.3</title>
-   </head>
-   <body>
-     <h1> Ejemplo 2.3</h1>
-     <canvas id="miCanvas" width ="100" height="100">
-       &iexc1;No funciona!
-     </canvas>
-     <script>
        var miCanvas=document.getElementById("myCanvas");
        var contexto=miCanvas.getContext("2d");
      contexto.fillStyle="red";
      contexto.fillRect(30, 30, 50, 50);
-   </body>
-   </html>
