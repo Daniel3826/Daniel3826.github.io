300e970ca90da47a328bdd53a919ab847ef10f42
Daniel3826 <dani.aguilar1201@gmail.com>
2017-02-20 00:56:32 -0600

    Update 20162.js

--- 20162.js
+++ 20162.js
@@ -4,11 +4,11 @@ contexto.fillStyle = "red";
 contexto.fillRect(10, 10, 50, 50);
 
 var miCanvas2 = document.getElementById("miCanvas2")
-var contexto = miCanvas2.getContext("2d");
+var contexto = miCanvas.getContext("2d");
 contexto.fillStyle = "blue";
 contexto.fillRect(300, 300, 50, 50);
 
 var miCanvas2 = document.getElementById("miCanvas2")
-var contexto = miCanvas2.getContext("2d");
+var contexto = miCanvas.getContext("2d");
 contexto.fillStyle = "yellow";
 contexto.fillRect(350, 350, 250, 250);
