e89dfe4d413384849c42265644c06344aa6fbb2e
Daniel3826 <dani.aguilar1201@gmail.com>
2017-03-01 09:03:51 -0600

    Update personas.js

--- personas.js
+++ personas.js
@@ -21,7 +21,8 @@ var geometriaCono1 = new THREE.ConeGeometry( 38, 50, 102 );
 var geometriaCono2 = new THREE.ConeGeometry( 38, 50, 102 );
 
 var forma1 = new THREE.CylinderGeometry( 10, 20, 60, 32 );
-var forma2 = new THREE.SphereGeometry(0.25);
+var forma2 = new THREE.SphereGeometry(28,32,32);
+forma2.translate(0,1,0);
 var forma1Malla = new THREE.Mesh(forma1);
 var forma2Malla = new THREE.Mesh(forma2);
 forma2Malla.position.x=100;
