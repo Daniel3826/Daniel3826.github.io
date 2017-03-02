e47abc68a14d7221dd8ee0aad802785e1ed41809
Daniel3826 <dani.aguilar1201@gmail.com>
2017-03-01 22:23:53 -0600

    Update personas.js

--- personas.js
+++ personas.js
@@ -120,7 +120,7 @@ function renderizar(){
 	cubo1.rotation.y += Math.PI * 0.5 / 180;
 	cubo1.rotation.z += Math.PI * Math.cos(x++ / 50) / 180;
 	
-	torus.rotation.x += Math.PI/2; 
+	torus.rotation.x += Math.PI*0.5/180; 
 	cone1.rotation.x += Math.PI;
 	cone2.rotation.x += Math.PI;
 	
