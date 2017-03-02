d5f932f7488c34e3c854e3480f82d9f9c50c665e
Daniel3826 <dani.aguilar1201@gmail.com>
2017-03-02 01:06:34 -0600

    Update personas.js

--- personas.js
+++ personas.js
@@ -132,8 +132,9 @@ escena.add(torus);
 escena.add(torus2);
 
 var camara = new THREE.PerspectiveCamera(120,(WIDTH / HEIGHT),0.5,10000);
-camara.position.z = 300;
-camara.position.y = 300;
+camara.position.z = 100;
+camara.position.y = 100;
+camara.position.x = 100;
 
 malla.position.x=-20;
 malla2.position.x=-20;
