284fe0d41dd7ffb4cef9cc8db477946c24152ec4
Daniel3826 <dani.aguilar1201@gmail.com>
2017-03-20 17:53:23 -0600

    Update Practica_20_03.js

--- Practica_20_03.js
+++ Practica_20_03.js
@@ -119,7 +119,7 @@ camara.right=window.innerWidth/2;
 camara.top=window.innerHeight/2;
 camara.bottom=window.innerHeight/-2;
 camara.near=100;
-camara.far=160;
+camara.far=200;
 camara.updateProjectionMatrix();
 
 camara.position.z=100;
