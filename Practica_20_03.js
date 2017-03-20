660dd6b66ae8615289107045ad39b7748000b985
Daniel3826 <dani.aguilar1201@gmail.com>
2017-03-20 16:22:49 -0600

    Update Practica_20_03.js

--- Practica_20_03.js
+++ Practica_20_03.js
@@ -112,18 +112,13 @@ var base4Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
   peonMalla1.translateY(3);
   peonMalla1.translateZ(-60);
 
-
 //Tablero
 var campoVision = 45;
-var relacionAspecto = window.innerWidth / window.innerHeight;
+var relacionAspecto = window.innerWidth / window.innerWidth;
 var planoCercano = 1;
 var planoLejano = 1000;
 var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
-camara.position.z=50;
-camara.position.x=160;
-camara.position.y=40;
-camara.lookAt(new THREE.Vector3(40,40,0));
-camara.rotateZ(Math.PI/2);
+camara.position.z=15;
 
 var escena = new THREE.Scene();
 var cubo= new Array();
