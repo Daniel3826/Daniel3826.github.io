c638ea90c7344490558740d1318ebb79565371b3
Daniel3826 <dani.aguilar1201@gmail.com>
2017-03-20 18:11:08 -0600

    Update Practica_20_03.js

--- Practica_20_03.js
+++ Practica_20_03.js
@@ -114,10 +114,10 @@ var base4Forma = new THREE.CylinderGeometry(5,5,1,20,1,false);
 
 //Tablero
 var camara = new THREE.OrthographicCamera();
-camara.left=window.innerWidth/-2.5;
-camara.right=window.innerWidth/-2.5;
-camara.top=window.innerHeight/-2.5;
-camara.bottom=window.innerHeight/-2.5;
+camara.left=window.innerWidth/2;
+camara.right=window.innerWidth/2;
+camara.top=window.innerHeight/2;
+camara.bottom=window.innerHeight/2;
 camara.near=1;
 camara.far=100;
 camara.updateProjectionMatrix();
